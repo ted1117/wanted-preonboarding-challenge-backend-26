1791e716338411a0b2f6951ead30cf95a8785650
ted111712 <ted111712@gmail.com>
2024-11-27 14:39:22 +0900

    Feat: User 회원 관리 기능 추가
    - 회원가입, 탈퇴, 로그인, 로그아웃

--- user/models.py
+++ user/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, nickname):
+        if not email:
+            raise ValueError("must have user email")
+        user = self.model(
+            email=self.normalize_email(email=email),
+            nickname=nickname or email.split("@")[0],
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, nickname):
+        user = self.create_user(email=email, password=password, nickname=nickname)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.BigAutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(unique=True, max_length=20)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["nickname"]
+
+    def __str__(self) -> object:
+        return self.nickname
--- user/views.py
+++ user/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from user.models import User
+from user.serializers import UserSchema, UserSerializer
+
 
 # Create your views here.
+class UserViewset(ModelViewSet):
+    serializer_class = UserSchema
+    queryset = User.objects.all()
+    serializer_classes = {
+        "list": UserSchema,
+        "retrieve": UserSchema,
+        "create": UserSerializer,
+        "delete": UserSerializer,
+        "update": UserSerializer,
+        "partial_update": UserSerializer,
+    }
+
+    def get_permissions(self):
+        if self.action in ["create", "login"]:
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.serializer_classes)
+
+    @action(methods=["post"], detail=False)
+    def login(self, request, *args, **kwargs):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        return Response(
+            {"message": "Invalid credentials."}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    @action(methods=["post"], detail=False)
+    def logout(self, request, *args, **kwargs):
+        logout(request)
+        return Response({"message": "Logout successful."}, status=status.HTTP_200_OK)
